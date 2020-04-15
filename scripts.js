a0c087447febb3545ff441fcf05909d537cb9f9d
3.14tyu <istvan.varga79@hotmail.com>
2020-04-15 16:37:23 +0200

    Some styling and fixing
    - fixed the <svg> on the button
    - no blue lines when button is active
    - make the arrows working
    - let the page more stylish

--- scripts.js
+++ scripts.js
@@ -14,6 +14,30 @@ let photos = [{
 
 let currentPhoto = 0;
 
-$('#photo').attr('src', photos[currentPhoto].photo);
+/*$('#photo').attr('src', photos[currentPhoto].photo);
 $('#pTitle').text(photos[currentPhoto].pTitle);
-$('#pDescription').text(photos[currentPhoto].pDescription);
+$('#pDescription').text(photos[currentPhoto].pDescription);*/
+
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', photos[photoNumber].photo);
+    $('#pTitle').text(photos[photoNumber].pTitle);
+    $('#pDescription').text(photos[photoNumber].pDescription);
+  }
+
+  loadPhoto(currentPhoto);
+
+  $('#rightButton').click(() => {
+    currentPhoto++;
+    if (currentPhoto > 2) {
+        currentPhoto = 0;
+    }
+    loadPhoto(currentPhoto);
+  })
+
+  $('#leftButton').click(() => {
+    currentPhoto--;
+    if (currentPhoto < 0) {
+        currentPhoto = 2;
+    }
+    loadPhoto(currentPhoto);
+  })
