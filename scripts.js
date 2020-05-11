9f31f9cf2ab8d3f5edfbc490345a994bc2b0f3fb
3.14tyu <istvan.varga79@hotmail.com>
2020-05-11 16:08:08 +0200

    new thumbnail version

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 let photos = [{
+  id: 1,
   photo: './img/eggs.jpeg',
   pTitle: 'Tojások',
-  pDescription: 'Tojáshélyakon járok; ezt már az elején tudtam, hogy nem lesz könnyű...'
+  pDescription: 'Tojáshéjakon járok; ezt már az elején tudtam, hogy nem lesz könnyű...'
 }, {
+  id: 2,
   photo: './img/hard_work.jpeg',
   pTitle: 'Az út a cél',
-  pDescription: 'Kemény munkával sikerüt a kitűzött célt megközelíteni. Sokat tanultam!'
-}, {
+  pDescription: 'Kemény munkával sikerült a kitűzött célt megközelíteni. Sokat tanultam!'
+}, 
+{
+  id: 3,
   photo: './img/moon.jpeg',
   pTitle: 'Erőforrás',
   pDescription: 'Telihold előtt mindig feltöltődöm energiával, olyan vagyok olyankor mint egy lelőhetetlen bagoly'
-}];
+},
+{
+  id: 4,
+  photo: './img/enter_the_new_world.jpeg',
+  pTitle: 'ÚjVilág',
+  pDescription: 'Aztaaaaaaa'
+},
+];
 
 let currentPhoto = 0;
 
@@ -18,7 +29,7 @@ let loadPhoto = (photoNumber) => {
   $('#photo').attr('src', photos[photoNumber].photo);
   $('#pTitle').text(photos[photoNumber].pTitle);
   $('#pDescription').text(photos[photoNumber].pDescription);
-  if (photoNumber === 0) {
+  /*if (photoNumber === 0) {
     $('#firstThumbnail').css('box-shadow', '0px 10px 10px 0px rgb(49, 49, 49)');
     $('#firstThumbnail::after').css('display','none');
     $('#secondThumbnail').css('box-shadow', 'none');
@@ -32,14 +43,26 @@ let loadPhoto = (photoNumber) => {
     $('#thirdThumbnail').css('box-shadow', '0px 10px 10px 0px rgb(49, 49, 49)');
     $('#firstThumbnail').css('box-shadow', 'none');
     $('#secondThumbnail').css('box-shadow', 'none');
-  }
+  }*/
 }
+
+photos.forEach((item, index) => {
+  $('.thumbnails').append(`<div class="thumbnail" photos-index="${index}">${item.id} (data-index="${index}") ${item.photo}</div>`);
+  $('.thumbnail').css('background-image','url('+item.photo+')');
+  console.log(index);
+  console.log(item.id);
+  console.log(item.photo);
+  // $('.thumbnail').click((event) => {
+  //   let indexClicked = $(event.target).attr('photos-index');
+  // });
+});
+
 
 loadPhoto(currentPhoto);
 
 $('#rightButton').click(() => {
   currentPhoto++;
-  if (currentPhoto > 2) {
+  if (currentPhoto > photos.length-1) {
     currentPhoto = 0;
   }
   loadPhoto(currentPhoto);
@@ -49,7 +72,7 @@ $('#rightButton').click(() => {
 $('#leftButton').click(() => {
   currentPhoto--;
   if (currentPhoto < 0) {
-    currentPhoto = 2;
+    currentPhoto = photos.length-1;
   }
   loadPhoto(currentPhoto);
 })
