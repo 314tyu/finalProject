1dea3f244f0b52853ff5fcafd6042a795828e86a
dantschi <dantschi@gmail.com>
2020-05-13 08:56:57 +0200

    special class handling is not perfect

--- scripts.js
+++ scripts.js
@@ -1,65 +1,113 @@
-let photos = [
-	{
-		id: 1,
-		photo: "./img/eggs.jpeg",
-		pTitle: "Tojások",
-		pDescription:
-			"Tojáshéjakon járok; ezt már az elején tudtam, hogy nem lesz könnyű...",
-	},
-	{
-		id: 2,
-		photo: "./img/hard_work.jpeg",
-		pTitle: "Az út a cél",
-		pDescription:
-			"Kemény munkával sikerült a kitűzött célt megközelíteni. Sokat tanultam!",
-	},
-	{
-		id: 3,
-		photo: "./img/moon.jpeg",
-		pTitle: "Erőforrás",
-		pDescription:
-			"Telihold előtt mindig feltöltődöm energiával, olyan vagyok olyankor mint egy lelőhetetlen bagoly",
-	},
-	{
-		id: 4,
-		photo: "./img/enter_the_new_world.jpeg",
-		pTitle: "ÚjVilág",
-		pDescription: "Aztaaaaaaa",
-	},
-];
+(function () {
+	$(document).ready(function () {
+		let photos = [
+			{
+				id: 1,
+				photo: "./img/eggs.jpeg",
+				pTitle: "Tojások",
+				pDescription:
+					"Tojáshéjakon járok; ezt már az elején tudtam, hogy nem lesz könnyű...",
+			},
+			{
+				id: 2,
+				photo: "./img/hard_work.jpeg",
+				pTitle: "Az út a cél",
+				pDescription:
+					"Kemény munkával sikerült a kitűzött célt megközelíteni. Sokat tanultam!",
+			},
+			{
+				id: 3,
+				photo: "./img/moon.jpeg",
+				pTitle: "Erőforrás",
+				pDescription:
+					"Telihold előtt mindig feltöltődöm energiával, olyan vagyok olyankor mint egy lelőhetetlen bagoly",
+			},
+			{
+				id: 4,
+				photo: "./img/enter_the_new_world.jpeg",
+				pTitle: "ÚjVilág",
+				pDescription: "Aztaaaaaaa",
+			},
+		];
+		let currentPhoto = 0;
 
-let currentPhoto = 0;
+		photos.forEach((item, index) => {
+			$(".thumbnails").append(
+				`<div class="thumbnail" data-photo-id="${item.id}" photos-index="${item.id}"></div>`
+			);
+			$('[photos-index="' + item.id + '"]').css(
+				"background-image",
+				"url(" + item.photo + ")"
+			);
+		});
 
-let loadPhoto = (photoNumber) => {
-	$("#photo").attr("src", photos[photoNumber].photo);
-	$("#pTitle").text(photos[photoNumber].pTitle);
-	$("#pDescription").text(photos[photoNumber].pDescription);
-};
+		let handleSpecialClass = () => {
+			console.log($(".thumbnail"));
 
-photos.forEach((item, index) => {
-	$(".thumbnails").append(
-		`<div class="thumbnail" photos-index="${item.id}">${item.id} (data-index="${index}") ${item.photo}</div>`
-	);
-	$('[photos-index="' + item.id + '"]').css(
-		"background-image",
-		"url(" + item.photo + ")"
-	);
-});
+			[...$(".thumbnail")].forEach((item) => {
+				if (
+					photos[currentPhoto].id ===
+					parseInt($(item).attr("data-photo-id"))
+				) {
+					item.classList.add("special");
+				} else {
+					item.classList.remove("special");
+				}
+			});
+		};
 
-loadPhoto(currentPhoto);
+		let loadPhoto = function (item) {
+			$("#photo").attr({
+				src: item ? item.photo : photos[currentPhoto].photo,
+				"data-photo-id": item ? item.id : photos[currentPhoto].id,
+			});
+			$("#pTitle").text(item ? item.pTitle : photos[currentPhoto].pTitle);
+			$("#pDescription").text(
+				item ? item.pDescription : photos[currentPhoto].pDescription
+			);
+			handleSpecialClass();
+		};
 
-$("#rightButton").click(() => {
-	currentPhoto++;
-	if (currentPhoto > photos.length - 1) {
-		currentPhoto = 0;
-	}
-	loadPhoto(currentPhoto);
-});
+		$(".thumbnail").click(() => {
+			// console.log($(event.target)[0].attributes);
+			// console.log($(event.target).attr("data-photo-id"));
 
-$("#leftButton").click(() => {
-	currentPhoto--;
-	if (currentPhoto < 0) {
-		currentPhoto = photos.length - 1;
-	}
-	loadPhoto(currentPhoto);
-});
+			photos.forEach((item) => {
+				if (
+					item.id === parseInt($(event.target).attr("data-photo-id"))
+				) {
+					loadPhoto(item);
+					handleSpecialClass();
+				}
+			});
+
+			// loadPhoto(event.target)
+		});
+
+		loadPhoto();
+
+		let loadNextPhoto = () => {
+			currentPhoto++;
+			if (currentPhoto > photos.length - 1) {
+				currentPhoto = 0;
+			}
+			loadPhoto();
+		};
+
+		let loadPreviousPhoto = () => {
+			currentPhoto--;
+			if (currentPhoto < 0) {
+				currentPhoto = photos.length - 1;
+			}
+			loadPhoto();
+		};
+
+		$("#rightButton").click(() => {
+			loadNextPhoto();
+		});
+
+		$("#leftButton").click(() => {
+			loadPreviousPhoto();
+		});
+	});
+})();
